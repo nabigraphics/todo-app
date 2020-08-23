3d07f6d522157e67ed76bbfad838990440cb0547
Jusung Hwang(nabigraphics) <hbh2692@naver.com>
2020-08-23 18:14:24 +0900

    Updates

---
+++ precache-manifest.b0b926bcaf321ad914e65dcaf72c0d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ba3a2b656e21e67900d22965aa144f",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8c9720acfab608441f91",
+    "url": "/todo-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8c9720acfab608441f91",
+    "url": "/todo-app/static/js/2.946032ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.946032ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57a911aabe5ca955f8e",
+    "url": "/todo-app/static/js/main.7ca6c49f.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
